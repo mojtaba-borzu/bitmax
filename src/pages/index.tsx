79fb9e245a44dc2a7671b7c6232423ec0b3b5cbc
mojtaba-borzu <mojtaba.borzu69@gmail.com>
2022-04-17 00:06:46 +0430

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,149 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-
-import Counter from '../features/counter/Counter'
-import styles from '../styles/Home.module.css'
+//libraries
+import Head from "next/head";
+import type { NextPage } from "next";
+import React, { useState, useCallback, useEffect } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 const IndexPage: NextPage = () => {
+  const [socketUrl, setSocketUrl] = useState(
+    "wss://ws.bitmex.com/realtime?subscribe=orderBookL2_25"
+  );
+  const [listAsset, setListAsset] = useState([]);
+  const [numCountList, setNumCountList] = useState(6);
+  const [messageHistory, setMessageHistory] = useState<any>([]);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+  useEffect(() => {
+    if (lastMessage !== null && messageHistory.length <= 2) {
+      setMessageHistory([...messageHistory, lastMessage]);
+    }
+
+    // splice(index, 1, item);
+
+    if (
+      lastMessage !== null &&
+      messageHistory.length == 3 &&
+      listAsset[1] &&
+      JSON.parse(lastMessage.data).data
+    ) {
+      // let f = listAsset.filter(
+      //   (item) =>
+      //     JSON.parse(lastMessage.data).data[0].price != "" &&
+      //     JSON.parse(lastMessage.data).data[0].price != undefined &&
+      //     JSON.parse(lastMessage.data).data[0].price != null &&
+      //     item.symbol == JSON.parse(lastMessage.data).data[0].symbol &&
+      //     item.price < JSON.parse(lastMessage.data).data[0].price
+      // );
+
+      // console.log(f);
+
+      let b;
+      listAsset
+        .filter(
+          (item) =>
+            JSON.parse(lastMessage.data).data[0].price != "" &&
+            JSON.parse(lastMessage.data).data[0].price != undefined &&
+            JSON.parse(lastMessage.data).data[0].price != null &&
+            item.symbol == JSON.parse(lastMessage.data).data[0].symbol
+        )
+        .map(
+          (row) =>
+            (b = listAsset.indexOf(row) == 0 ? null : listAsset.indexOf(row))
+        );
+      listAsset.splice(b, 1, JSON.parse(lastMessage.data).data[0]);
+    }
+  }, [lastMessage]);
+
+  useEffect(() => {
+    if (messageHistory.length == 3) {
+      let c = [{ id: 0 }];
+      messageHistory
+        .slice(2, 3)
+        .map((message) =>
+          c.push(
+            ...removeSimilarData(JSON.parse(message.data).data).slice(
+              0,
+              numCountList
+            )
+          )
+        );
+      setListAsset(c);
+    }
+  }, [messageHistory, numCountList]);
+
   return (
-    <div className={styles.container}>
+    <div className=" w-screen h-screen">
       <Head>
-        <title>Redux Toolkit</title>
+        <title>bitmax</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
-        <img src="/logo.svg" className={styles.logo} alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className={styles.link}
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="w-full flex flex-col items-center px-[20px] py-[20px]">
+        <div className="w-full h-[100px] flex justify-center gap-4">
+          <button
+            onClick={() =>
+              numCountList <= 25 && setNumCountList(numCountList + 5)
+            }
+            className="border w-[150px] h-[40px] rounded-xl"
           >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+            Add +5 Column
+          </button>{" "}
+          <button
+            onClick={() =>
+              numCountList > 6 && setNumCountList(numCountList - 5)
+            }
+            className="border w-[150px] h-[40px] rounded-xl"
           >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+            Add -5 Column
+          </button>
+        </div>
+        <div className="py-[20px] w-full flex justify-center">
+          {`All Assets : ${numCountList - 1}`}
+        </div>
+        <table className="">
+          <thead className="rounded-t-xl">
+            <tr className="border text-center rounded-t-xl">
+              <th className="border-r w-[100px]">coin</th>
+              <th className="border-r w-[100px]">price</th>
+              <th className="border-r w-[100px]">side</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listAsset.slice(1, numCountList).map((item, idx) => (
+              <tr key={idx}>
+                <td className="">
+                  <div className="border-l border-t border-b rounded-l-xl  h-[50px] flex items-center justify-center">
+                    {item.symbol}
+                  </div>
+                </td>
+                <td className="">
+                  <div className=" border-t border-b h-[50px] flex items-center justify-center">
+                    {item.price}
+                  </div>
+                </td>
+                <td className="">
+                  {" "}
+                  <div className="border-r border-t border-b rounded-r-xl  h-[50px] flex items-center justify-center">
+                    {item.side}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
+
+function removeSimilarData(assetList) {
+  const uniqueValuesSet = new Set();
+  let filteredArr = assetList.filter((obj) => {
+    const isPresentInSet = uniqueValuesSet.has(obj.symbol);
+    uniqueValuesSet.add(obj.symbol);
+    return !isPresentInSet;
+  });
+
+  return filteredArr;
+}
